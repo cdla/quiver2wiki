a2ca79e8cda09f5020ef5ac0ed34b325e54bd3ef
Carlo de los Angeles <carlo.delosangeles@gmail.com>
2018-12-12 23:24:54 -0800

    added main function

---
+++ quiver2wiki/main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+import argparse
+from quiver2wiki.scripts import export2md
+
+
+def main():
+
+    parser = argparse.Argument Parser(
+    description = 'helper tool for making quiver libraries into wikis')
+
+    parser.add_argument("--export",dest='export', help="export quiver library into markdown files", type=str, default=None)
+#    parser.add_argument("--host",dest='host',
+#    help="host the wiki formed from the quiver library", type=str, default=None)
+
+    try:
+        args = parser.parse_args()
+    except:
+        parser.print_help()
+        sys.exit(0)
+
+    if args.export != None:
+        export2md(args.export)
+    else:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
