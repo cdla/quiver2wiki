e7781b49719b75c6135f0a5194a58355d34d6451
Carlo de los Angeles <carlo.delosangeles@gmail.com>
2018-12-31 22:41:21 -0800

    feature list_pages completed

--- quiver2wiki/main.py
+++ quiver2wiki/main.py
@@ -1,7 +1,6 @@
 import os
 import sys
 import argparse
-from quiver2wiki.scripts import export2md
 from quiver2wiki.scripts import utils
 
 def main():
@@ -27,8 +26,21 @@ def main():
         print("The list of notebooks within the quiver library at %s is: "%(args.quiver_lib))
         for nb in nb_list:
             print(nb)
-    elif args.export != None:
-        export2md(args.export)
+    elif args.list_pages == True:
+        nb_list,nb_dict=utils.list_notebooks(args.quiver_lib)
+        page_dict=utils.list_pages(args.quiver_lib,nb_dict)
+
+        print("The page organization for the quiver library at %s is: "%args.quiver_lib)
+
+        for page in page_dict:
+            print("Quiver Notebook: %s"%page)
+            if len(page_dict[page]) == 0:
+                print("    There are no pages within this notebook")
+            else:
+                for page in page_dict[page]:
+                    print("    - %s "%page)
+
+
     else:
         parser.print_help()
 
--- quiver2wiki/scripts/utils.py
+++ quiver2wiki/scripts/utils.py
@@ -6,14 +6,44 @@ def list_notebooks(quiver_location,private_list=[]):
     ''' list_notebooks will return a list of the notebooks, ignoring the private notebooks
     :param quiver_location: the location of the quiver library of notebooks
     :param private_list: the list of private notebooks to ignore
+
+    TODO: add ignore private_list functionality
     '''
     meta_json = os.path.join(quiver_location,'meta.json')
 
     nb_meta_jsons=glob(os.path.join(quiver_location,'*','meta.json'))
     nb_list = []
+    nb_dict = dict()
     for nb_meta_json in nb_meta_jsons:
         with open(nb_meta_json,'r') as meta_file:
             meta = json.load(meta_file)
             nb_list.append(meta['name'])
+            nb_dict[meta['name']]=meta
+    return nb_list,nb_dict
 
-    return nb_list
+def list_pages(quiver_location,nb_dict, private_list=[]):
+    ''' list_pages will return a list of pages of the notebooks, ignoring the private notebooks
+    :param quiver_location: the location of the quiver library of notebooks
+    :param nb_dict: a dictionary of the quiver notebooks within the library
+    :param private_list: the list of private notebooks to ignore
+
+    TODO: add ignore private_list functionality
+    TODO: add function for further recursive notebooks
+    '''
+
+    nb_page_dict=dict()
+
+    for nb in nb_dict:
+        nb_page_list=dict()
+
+        nb_page_list[nb_dict[nb]['name']]=[]
+        page_meta_jsons=glob(os.path.join(quiver_location,nb_dict[nb]['uuid'] + ".qvnotebook",'*','meta.json'))
+
+        for page_meta_json in page_meta_jsons:
+            with open(page_meta_json,'r') as meta_file:
+                meta = json.load(meta_file)
+                nb_page_list[nb_dict[nb]['name']].append(meta['title'])
+
+        nb_page_dict[nb] = sorted(nb_page_list[nb])
+
+    return nb_page_dict
